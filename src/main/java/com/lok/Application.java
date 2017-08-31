6890bce5a08e542be0e57b25a150fe468713ac6f
coxujiale <574262853@qq.com>
2017-08-31 18:34:52 +0800

    second test

--- src/main/java/com/lok/Application.java
+++ src/main/java/com/lok/Application.java
@@ -1,9 +1,16 @@
 package com.lok;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
 /**
  * @Description 启动类
  */
 
+@SpringApplicationConfiguration
 public class Application {
-
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
