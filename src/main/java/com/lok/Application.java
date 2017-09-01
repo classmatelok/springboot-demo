c613b5acfb6c0c03c8c5cf85c1d9eb2f63d25fe1
Lok <classmatelok@163.com>
2017-09-01 16:47:56 +0800

    Update Application.java

--- src/main/java/com/lok/Application.java
+++ src/main/java/com/lok/Application.java
@@ -22,6 +22,6 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args); 
+		SpringApplication.run(Application.class, args);
 	}
 }
