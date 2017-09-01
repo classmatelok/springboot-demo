3566170cc195fd49674d20fc9e276a4ea7053834
coxujiale <574262853@qq.com>
2017-09-01 16:09:35 +0800

    初步搭建springboot,访问success

--- src/main/java/com/lok/Application.java
+++ src/main/java/com/lok/Application.java
@@ -1,13 +1,24 @@
 package com.lok;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+*/
 
 /**
  * @Description 启动类
  */
 
-@SpringApplicationConfiguration
+/*
+//@SpringBootApplication相当于以下三个
+@Configuration // 默认加载classpath下的application.properties
+@EnableAutoConfiguration
+@ComponentScan // 可指定包，默认扫启动类的同包及子包
+*/
+@SpringBootApplication
 public class Application {
 	
 	public static void main(String[] args) {
