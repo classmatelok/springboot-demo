1df61e21f81f9737b588d0af87ced6428e48db90
coxujiale <574262853@qq.com>
2017-09-19 10:58:26 +0800

    改数据来源，变dead code返回，等数据库准备好再改连数据库

--- src/main/resources/resources/js/user.js
+++ src/main/resources/resources/js/user.js
@@ -6,14 +6,27 @@ $(function(){
 			//data:,
 			async:true,
 			dataType:"json",
-			success:function(resData){
-				if(resData.status==1){
-					$("#show").html(data);
+			success:function(resData,textStatus,jqXHR){
+				//console.log(resData);
+				//console.log(jqXHR.status);
+				var tr = "";
+				if(resData.status=='1'){
+					$.each(resData.data,function(i,item){
+						tr += "<tr>";
+						tr += "<td>"+item.id+"</td>";
+						tr += "<td>"+item.name+"</td>";
+						tr += "<td>"+item.sex+"</td>";
+						tr += "<td>"+item.age+"</td>";
+						tr += "</tr>";
+						
+					});
+					$("#content").html(tr);
 				}else{
 					alert("服务器繁忙，请稍后再试！");
 				}
 			},
 			error:function(){
+				alert("出错了！");
 				
 			}
 		});
