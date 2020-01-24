f2af5254f11256de50fdf55654ee45aa8885e8f2
lok <lok@qq.com>
2020-01-24 20:17:50 +0800

    add for BigDeciamlTools

--- src/main/java/com/lok/utils/BigDecimalTools.java
+++ src/main/java/com/lok/utils/BigDecimalTools.java
@@ -1,6 +1,7 @@
 package com.lok.utils;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * @author Lok
@@ -22,7 +23,9 @@ public class BigDecimalTools {
 	
 	private static String multiplyDecimal(String num1Str, String num2Str) {
 		BigDecimal num1 = new BigDecimal(num1Str);
-		return num1.multiply(new BigDecimal(num2Str)).toPlainString();//取消科学计数显示模式
+		return num1.multiply(new BigDecimal(num2Str)).setScale(2, RoundingMode.HALF_UP)//精确到2位小数，并四舍五入
+				.stripTrailingZeros()//e.g.这里代表把不足2位小数多余的0去掉
+				.toPlainString();//取消科学计数显示模式
 	}
 	
 	private static String divideDecimal(String num1Str, String num2Str) {
